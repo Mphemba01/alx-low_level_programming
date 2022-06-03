522ab18ee47c2acbcb676194d1ac00f2fa795f43
Mphemba01 <bbn,ntuli@gmail.com>
2022-06-02 20:24:05 -0700

    Task 4 edited part 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * 	* main - Entry point
+ *	 *	*
+ * 	  *Return: Always 0 (Success)
+ *	   **/
 int main(void)
 {
-	puts("\"Progamming is like building a multilingual puzzle*);
+	puts("\"Progamming is like building a multilingual puzzle");
 		return (0);
 }
