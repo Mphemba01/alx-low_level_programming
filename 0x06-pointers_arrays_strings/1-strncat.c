5d6239454688c5df4904a710512d4421efb527a1
Mphemba01 <bbn,ntuli@gmail.com>
2022-06-16 14:27:11 -0700

    Task 1 fixing an error for Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,24 +9,24 @@
 
 char *_strncat(char *dest, char *src, int n)
 
-	{
-	int i, j;
+{
+int i, j;
 
-	i = 0;
-	while (dest[i] != '\0')
-		{
-		i++;
-		}
-	
-	j = 0;
-	while (j < n && src[j] != '\0')
-		{
-		dest[i] = src[j];
-		j++;
-		i++;
-		}
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
 
-	dest[i] = '\0';
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
 
-	return (dest);
-	}
+dest[i] = '\0';
+
+return (dest);
+}
