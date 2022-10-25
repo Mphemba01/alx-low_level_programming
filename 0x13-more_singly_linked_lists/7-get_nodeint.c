98f655160921c32274fe60368bad9945731c76eb
Brutus Bonginkosi <bbn.ntuli@gmail.com>
2022-10-25 16:26:18 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index -returns the nth node of a link list
+ * get_nodeint_at_index - returns the nth node of a linked list
  * @head: head of a list.
  * @index: index of the node.
  *
@@ -9,10 +9,9 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-
 	unsigned int i;
 
-	for (i = 0; i < index && head !=NULL; i++)
+	for (i = 0; i < index && head != NULL; i++)
 	{
 		head = head->next;
 	}
