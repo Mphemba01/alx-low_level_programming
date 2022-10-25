fcb75c16f662db6a9c43132b15e896b450312022
Brutus Bonginkosi <bbn.ntuli@gmail.com>
2022-10-25 16:41:19 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,15 +9,14 @@
  */
 int sum_listint(listint_t *head)
 {
-
 	int sum;
 
 	sum = 0;
-	while (head !=NULL)
+	while (head != NULL)
 	{
-		sum +=head ->n;
-		head = head ->next;
+		sum += head->n;
+		head = head->next;
 	}
 
-	return (sum) ;
+	return (sum);
 }
